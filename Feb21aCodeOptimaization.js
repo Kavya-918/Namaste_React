e19e3bec051cc71335463efef745b756b9a80a71
P K KAVYA <132293399+Kavya-918@users.noreply.github.com>
2025-02-20 22:46:53 -0800

    Update Feb21aCodeOptimaization.js

--- Feb21aCodeOptimaization.js
+++ Feb21aCodeOptimaization.js
@@ -15,7 +15,31 @@ const RestaurantCard = (props) =>{
         </div>
     );
 }; 
-// above part need to optimazation 
+// above part need to optimazation to
+    const{
+      cloudinaryImageId,
+      name,
+      cuisines,
+      avgRating,
+      costForTwo,
+      deliveryTime} = resData ?.info;
+    return(
+        
+        <div className="res-card" style={{ backgroundColor : "#f0f0f0"}}>
+            <img className="res-logo"
+            alt ="res-logo"
+             src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+             resData.info.cloudinaryImageId} />
+            <h3>{name}</h3>
+            <h4>{cuisines.join(",")}</h4>
+            <h4>{avgRating}</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{deliveryTime}</h4>
+
+        </div>
+    );
+};
+//end of above optimazation
 
 
 //full code: for refer
