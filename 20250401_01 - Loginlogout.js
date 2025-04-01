a963b9932a0bf950a6f6f1e1f6b53ba17d4c4017
P K KAVYA <132293399+Kavya-918@users.noreply.github.com>
2025-04-01 00:58:58 -0700

    Update 20250401_01 - Loginlogout.js

--- 20250401_01 - Loginlogout.js
+++ 20250401_01 - Loginlogout.js
@@ -83,3 +83,46 @@ export default Header;
 Note:
 btnNameReact is a constant how can modify from login to logout?
 ans: Whenever you update the setBtnNamReact react is updating the btnNameReact and calling the header once again or rendering with or as "New Vaeiable" this is the beauty of react
+
+
+
+
+//again login if logout
+
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+
+const Header = () => {
+    const [btnNameReact,setBtnNameReact] = useState("Login");
+    return (
+        <div className="header">
+            <div className="Logo-container">
+                <img
+                    className="logo"
+                    src={LOGO_URL}
+                    alt="logo"
+                />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                    <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;  
